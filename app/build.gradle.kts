e5f78a51285d725c181905ba5730b7fc8fd79c1c
ylose <heungeob1003@gmail.com>
2023-09-17 18:24:15 +0900

    firebase auth 23.09.17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
     implementation("androidx.databinding:databinding-runtime:8.1.1")
+    implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/shared_calendar/MainActivity.kt
+++ app/src/main/java/com/example/shared_calendar/MainActivity.kt
@@ -1,11 +1,36 @@
-package com.example.shared_calendar
+package com.example.capstonedesign
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.capstonedesign.auth.IntroActivity
+import com.example.shared_calendar.R
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        auth = FirebaseAuth.getInstance()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*findViewById<Button>(R.id.logoutBtn).setOnClickListener {
+
+            // 로그아웃
+            auth.signOut()
+
+            // IntroActivity로 화면 이동
+            val intent = Intent(this, IntroActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+
+        }*/
     }
 }
